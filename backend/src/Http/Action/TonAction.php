285fa946892545def7dbde533088c8a5a0178a59
adfaf <qwe@gmail.com>
2025-02-07 17:12:56 +0200

    qweeee

--- backend/src/Http/Action/TonAction.php
+++ backend/src/Http/Action/TonAction.php
@@ -6,14 +6,8 @@ namespace App\Http\Action;
 
 use App\Http\JsonResponse;
 use App\Services\TonApiClient;
-use App\Services\TonClient;
-use Http\Client\Common\HttpMethodsClient;
-use Http\Discovery\Psr17FactoryDiscovery;
-use Http\Discovery\Psr18ClientDiscovery;
 use Olifanton\Interop\KeyPair;
-use Olifanton\Mnemonic\TonMnemonic;
 use Olifanton\Ton\Contracts\Wallets\Wallet;
-use Olifanton\Ton\Transports\Toncenter\ClientOptions;
 use Olifanton\Ton\Transports\Toncenter\ToncenterHttpV2Client;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
@@ -21,31 +15,38 @@ use Psr\Http\Server\RequestHandlerInterface;
 
 readonly class TonAction implements RequestHandlerInterface
 {
-    private ToncenterHttpV2Client $tonCenterClient;
-    private Wallet $wallet;
-    private KeyPair $keyPair;
-
     public function __construct()
     {
     }
-
+// home 0QCxxdLsVRwdzoXYB9fC69cMStjhkbIbCC6glubn_hOHHwwq
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
-
         $mnemonic = 'fossil alert evoke travel relief amazing escape marine bread test fine ripple call hidden woman dice cruise wagon object tenant camera father dance merit';
+
         $client = new TonApiClient(
             tatumApiKey: '7b190682ac904de6e6e508cfbc86d8311ec41b7497f76a6b873b6c4f6149155d',
             mnemonic: explode(' ', $mnemonic),
             testMode: true
         );
 
-        $address = '0QCxxdLsVRwdzoXYB9fC69cMStjhkbIbCC6glubn_hOHHwwq'; // Тестовый адрес
-        $balance = $client->getBalance($address);
-        var_dump($balance);exit();
+        $address = '0QCciRsmZ-58lAichaN6_mMqZSJHTDq477hO52-sBXOQ26us'; // Тестовый адрес
 
-        $transactions = $client->getTransactions($address);
+        try {
+            $result = $client->sendTransaction(
+                address: '0QDqUgM7qGd0txRl_zxKA8Z7H2H4cnuaLj_gyLxXnlInpPOT',
+                amount: "0.2",
+                message: "start testing",
+            );
+        } catch (\Exception $e) {
+            echo "Error: " . $e->getMessage();
+        }
+        var_dump($result);exit();
+//        $balance = $client->getBalance($address)->toFloat();
+//        $wi = $client->getAddressInformation($address);
+//        var_dump($balance);exit();
+//        $transactions = $client->getTransactions($address);
 
-
+        exit();
         return new JsonResponse(['balance' => 'SUCCESS']);
     }
 }
--- backend/src/Services/TonApiClient.php
+++ backend/src/Services/TonApiClient.php
@@ -1,20 +1,25 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Services;
 
+use Brick\Math\BigNumber;
 use Http\Client\Common\HttpMethodsClient;
 use Http\Discovery\Psr17FactoryDiscovery;
 use Http\Discovery\Psr18ClientDiscovery;
 use Olifanton\Interop\Address;
+use Olifanton\Interop\Bytes;
 use Olifanton\Interop\KeyPair;
 use Olifanton\Interop\Units;
 use Olifanton\Mnemonic\TonMnemonic;
+use Olifanton\Ton\AddressState;
+use Olifanton\Ton\Contracts\Wallets\Transfer;
+use Olifanton\Ton\Contracts\Wallets\TransferOptions;
 use Olifanton\Ton\Contracts\Wallets\V4\WalletV4Options;
 use Olifanton\Ton\Contracts\Wallets\V4\WalletV4R2;
+use Olifanton\Ton\SendMode;
 use Olifanton\Ton\Transports\Toncenter\ClientOptions;
-use Olifanton\Ton\Transports\Toncenter\Exceptions\ClientException;
-use Olifanton\Ton\Transports\Toncenter\Exceptions\TimeoutException;
-use Olifanton\Ton\Transports\Toncenter\Exceptions\ValidationException;
 use Olifanton\Ton\Transports\Toncenter\ToncenterHttpV2Client;
 use Olifanton\Ton\Transports\Toncenter\ToncenterTransport;
 
@@ -23,13 +28,13 @@ class TonApiClient
     private ToncenterHttpV2Client $tonCenter;
     private WalletV4R2 $wallet;
     private KeyPair $keyPair;
-    private ToncenterTransport $transport;
 
     public function __construct(
         string $tatumApiKey,
-        array $mnemonic,
-        bool $testMode = false
-    ) {
+        array  $mnemonic,
+        bool   $testMode = false
+    )
+    {
         $httpClient = new HttpMethodsClient(
             Psr18ClientDiscovery::find(),
             Psr17FactoryDiscovery::findRequestFactory(),
@@ -44,43 +49,67 @@ class TonApiClient
             )
         );
 
-        $this->transport = new ToncenterTransport($this->tonCenter);
         $this->keyPair = TonMnemonic::mnemonicToKeyPair($mnemonic);
         $walletOptions = new WalletV4Options($this->keyPair->publicKey);
         $this->wallet = new WalletV4R2($walletOptions);
+
     }
 
-    public function getBalance(string $address)
+
+    public function getBalance(string $address): BigNumber
     {
         $balance = $this->tonCenter->getAddressBalance(new Address($address));
+
         return Units::fromNano($balance);
     }
 
-    public function getTransactions(string $address, int $limit = 20): array
+    public function sendTransaction(
+        string $address,
+        string $amount,
+        string $message
+    ): string
+    {
+        $transport = new ToncenterTransport($this->tonCenter);
+        $seQno = $this->wallet->seqno($transport);
+        $address = new Address($address);
+
+        // Создаем сообщение для трансфера
+        $transferMessage = $this->wallet->createTransferMessage(
+            [
+                new Transfer(
+                    $address,
+                    Units::toNano($amount),
+                    $message,
+                    SendMode::PAY_GAS_SEPARATELY,
+                )
+            ],
+            new TransferOptions($seQno ?? 0),
+        );
+
+        // Сначала подписываем сообщение
+        $signedMessage = $transferMessage->sign($this->keyPair->secretKey);
+
+        // Затем отправляем подписанное сообщение
+        $transport->send($signedMessage->toBoc(false));
+
+        // Возвращаем хеш транзакции
+        return Bytes::bytesToBase64($signedMessage->hash());
+    }
+
+    public function getTransactions(
+        string  $address,
+        ?int    $limit = null,
+        ?int    $lt = null,
+        ?string $hash = null,
+        ?int    $toLt = null
+    ): array
     {
         return $this->tonCenter->getTransactions(
             new Address($address),
-            $limit
+            $limit,
+            $lt,
+            $hash,
+            $toLt
         )->items;
-    }
-
-    /**
-     * @throws TimeoutException
-     * @throws ClientException
-     * @throws ValidationException
-     */
-    public function getAddressInformation(string $address)
-    {
-        return $this->tonCenter->getAddressInformation(new Address($address));
-    }
-
-    public function getMasterchainInfo()
-    {
-        return $this->tonCenter->getMasterchainInfo();
-    }
-
-    public function getWalletInformation()
-    {
-        return $this->tonCenter->getAddressInformation($this->wallet->getAddress());
     }
 }
